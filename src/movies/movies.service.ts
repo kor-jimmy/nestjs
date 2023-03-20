23d755015c40e9603aa55082ce059b2103b393f5
jintak.yoo <ascending77@naver.com>
2023-03-20 23:10:16 +0900

    api 예제 작성중

---
+++ src/movies/movies.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { Movie } from './entities/movie.entity';
+
+@Injectable()
+export class MoviesService {
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies;
+  }
+
+  getOne(movieId: string): Movie {
+    const movie = this.movies.find((m) => {
+      return m.id === +movieId;
+    });
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID : ${movieId} not found`);
+    }
+    return movie;
+  }
+
+  deleteOne(id: string) {
+    this.getOne(id);
+    this.movies = this.movies.filter((movie) => movie.id !== +id);
+    console.log(this.movies);
+  }
+
+  create(movieData: CreateMovieDto) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    });
+    return this.movies;
+  }
+
+  update(id: string, updateData) {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({ ...movie, ...updateData });
+  }
+}
